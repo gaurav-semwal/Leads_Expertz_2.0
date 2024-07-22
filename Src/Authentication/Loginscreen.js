b50dd06d9f62692737774598bda8602a7fa10e41
chaishta-tech <cheishta@clikzopinnovations.com>
2024-07-22 17:36:02 +0530

    sidebar

--- Src/Authentication/Loginscreen.js
+++ Src/Authentication/Loginscreen.js
@@ -1,21 +1,122 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, Pressable, StyleSheet, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Provider as Paperprovider, TextInput } from 'react-native-paper';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Button from '../Components/Button';
 
-const Loginscreen = ({navigation}) => {
+const Loginscreen = ({ navigation }) => {
 
-    const navigate=()=>{
-        navigation.navigate('bottom')
-    }
+  const [loading, setLoading] = useState(false);
+
+  const handleLoginPress = async () => {
+    navigation.navigate('AppDrawer');
+    // try {
+    //   setLoading(true);
+    //   const response = await Login(mobilenumner, password);
+    //   if (response.msg === "User logged in successfully.") {
+    //     await AsyncStorage.removeItem('authToken');
+
+    //     await AsyncStorage.setItem('authToken', response.data.token);
+    //     await AsyncStorage.setItem('userPassword', response.data.password);
+    //     await AsyncStorage.setItem('mobile', response.data.mobile);
+
+    //     Toast.show({
+    //       text1: 'User login successful',
+    //       type: 'success',
+    //     });
+
+    //     navigation.navigate('bottom');
+    //   } else {
+    //     Toast.show({
+    //       text1: 'Failed to login!',
+    //       type: 'error',
+    //     });
+    //   }
+    // } catch (error) {
+    //   console.log(error);
+    //   Toast.show({
+    //     text1: 'Error',
+    //     type: 'error',
+    //   });
+    // } finally {
+    //   setLoading(false);
+    // }
+  };
+
+
+  const [email, setemail] = useState();
+  const [password, setpassword] = useState();
+
   return (
-    <View>
-        <Pressable onPress={navigate}>
-        <Text>Loginscreen</Text>
+    <Paperprovider style={styles.container}>
+
+      {/* {loading && (
+        <View style={styles.loader}>
+          <ActivityIndicator size="large" />
+        </View>
+      )} */}
+
+      <View>
+        <Image
+          style={styles.logo}
+          resizeMode='contain'
+          source={require('../Assets/logo.png')}
+        />
+        <View style={styles.textinputview}>
+          <View>
+            <View style={{ position: 'absolute', top: 25, left: 13, zIndex: 1 }}>
+              <MaterialCommunityIcons name="email" size={25} color="#625bc5" />
+            </View>
+            <TextInput
+              placeholder="Enter Your Email"
+              value={email}
+              onChangeText={text => setemail(text)} 
+              style={[styles.textinput, { paddingLeft: 20 }]}
+              mode="outlined"
+            />
+          </View>
+          <View>
+            <View style={{ position: 'absolute', top: 25, left: 13, zIndex: 1 }}>
+              <MaterialCommunityIcons name="lock" size={25} color="#625bc5" />
+            </View>
+            <View>
+            <TextInput
+              placeholder="Enter Your Password"
+              value={password}
+              mode="outlined"
+              onChangeText={text => setpassword(text)} 
+              style={[styles.textinput, { paddingLeft: 20 }]}
+            />
+            </View>
+          </View>
+        </View>
+        <Pressable style={{ top: 20 }} onPress={handleLoginPress}>
+          <Button text="Login" />
         </Pressable>
-    
-    </View>
-  )
-}
+      </View>
+    </Paperprovider>
+  );
+};
 
-export default Loginscreen
+export default Loginscreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  eye: {
+    alignItems: 'flex-end',
+    bottom: '45%',
+    marginRight: "6%"
+  },
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  logo: {
+    height: '40%',
+    width: '80%',
+    alignSelf: 'center',
+  },
+  textinput: {
+    marginBottom: 10,
+    margin: 10,
+  },
+});
--- Src/Navigation/Stacknavigation.js
+++ Src/Navigation/Stacknavigation.js
@@ -1,26 +1,43 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Provider as PaperProvider } from 'react-native-paper';
 import Loginscreen from '../Authentication/Loginscreen';
-import { Provider } from 'react-native-paper';
-import Bottomnavigation from './Bottomnavigation';
+import Homescreen from '../Screens/Homescreen';
+import Drawercontent from './Drawercontent';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigation = () => {
+  return (
+    <Drawer.Navigator drawerContent={props => <Drawercontent {...props} />}>
+      <Drawer.Screen name="Dashboard" component={Homescreen} options={{
+        headerShown: true,
+        headerStyle: {
+          backgroundColor: '#625bc5',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }} />
+    </Drawer.Navigator>
+  );
+};
 
 const Stacknavigation = () => {
   return (
     <NavigationContainer>
-      <Provider>
-      <Stack.Navigator>
-      <Stack.Screen name="Login" component={Loginscreen} options={{ headerShown: false }} />
-      <Stack.Screen name="bottom" component={Bottomnavigation} options={{ headerShown: false }} />
-    </Stack.Navigator>
-      </Provider>
-  </NavigationContainer>
-  )
-}
+      <PaperProvider>
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={Loginscreen} options={{ headerShown: false }} />
+          <Stack.Screen name="AppDrawer" component={DrawerNavigation} options={{ headerShown: false }} />
+        </Stack.Navigator>
+      </PaperProvider>
+    </NavigationContainer>
+  );
+};
 
-export default Stacknavigation
-
-const styles = StyleSheet.create({})
+export default Stacknavigation;
