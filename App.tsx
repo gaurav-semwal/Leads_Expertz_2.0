f5e9ad811caf4390bc831de60cfdd87edb1d96df
gaurav-semwal <gauravsemwal8572@gmail.com>
2024-07-29 16:09:40 +0530

    Added Api

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
-import 'react-native-gesture-handler'
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import Stacknavigation from './Src/Navigation/Stacknavigation'
+import 'react-native-gesture-handler';
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import Stacknavigation from './Src/Navigation/Stacknavigation';
+import Toast from 'react-native-toast-message';
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Stacknavigation/>
+      <Stacknavigation />
+      <Toast />
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
 const styles = StyleSheet.create({
   container: {
-    flex:1
-  }
-})
+    flex: 1,
+  },
+});
--- Src/Authentication/Loginscreen.js
+++ Src/Authentication/Loginscreen.js
@@ -1,51 +1,108 @@
-import { Image, Pressable, StyleSheet, View } from 'react-native';
 import React, { useState, useEffect } from 'react';
+import { Image, Pressable, StyleSheet, View } from 'react-native';
 import { Provider as Paperprovider, TextInput } from 'react-native-paper';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Button from '../Components/Button';
+import Toast from 'react-native-toast-message';
+import { Login_Api } from '../../Api/authApi';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
 
 const Loginscreen = ({ navigation }) => {
 
   const [loading, setLoading] = useState(false);
 
-  const handleLoginPress = async () => {
-    navigation.navigate('AppDrawer');
-    // try {
-    //   setLoading(true);
-    //   const response = await Login(mobilenumner, password);
-    //   if (response.msg === "User logged in successfully.") {
-    //     await AsyncStorage.removeItem('authToken');
+  // const handleLoginPress = async () => {
+  //   navigation.navigate('AppDrawer');
+  //   // try {
+  //   //   setLoading(true);
+  //   //   const response = await Login(mobilenumner, password);
+  //   //   if (response.msg === "User logged in successfully.") {
+  //   //     await AsyncStorage.removeItem('authToken');
 
-    //     await AsyncStorage.setItem('authToken', response.data.token);
-    //     await AsyncStorage.setItem('userPassword', response.data.password);
-    //     await AsyncStorage.setItem('mobile', response.data.mobile);
+  //   //     await AsyncStorage.setItem('authToken', response.data.token);
+  //   //     await AsyncStorage.setItem('userPassword', response.data.password);
+  //   //     await AsyncStorage.setItem('mobile', response.data.mobile);
 
-    //     Toast.show({
-    //       text1: 'User login successful',
-    //       type: 'success',
-    //     });
+  //   //     Toast.show({
+  //   //       text1: 'User login successful',
+  //   //       type: 'success',
+  //   //     });
 
-    //     navigation.navigate('bottom');
-    //   } else {
-    //     Toast.show({
-    //       text1: 'Failed to login!',
-    //       type: 'error',
-    //     });
-    //   }
-    // } catch (error) {
-    //   console.log(error);
-    //   Toast.show({
-    //     text1: 'Error',
-    //     type: 'error',
-    //   });
-    // } finally {
-    //   setLoading(false);
-    // }
-  };
-
-
+  //   //     navigation.navigate('bottom');
+  //   //   } else {
+  //   //     Toast.show({
+  //   //       text1: 'Failed to login!',
+  //   //       type: 'error',
+  //   //     });
+  //   //   }
+  //   // } catch (error) {
+  //   //   console.log(error);
+  //   //   Toast.show({
+  //   //     text1: 'Error',
+  //   //     type: 'error',
+  //   //   });
+  //   // } finally {
+  //   //   setLoading(false);
+  //   // }
+  // };
   const [email, setemail] = useState();
   const [password, setpassword] = useState();
+
+  useEffect(() => {
+    checkForToken(); 
+  }, []);
+
+  const checkForToken = async () => {
+    const token = await AsyncStorage.getItem('authToken');
+    
+    if (token) {
+      navigation.navigate('AppDrawer');
+    }
+  };
+
+  const handleLoginPress = async () => {
+    try {
+      if (!email || !password) {
+        Toast.show({
+          text1: 'Please enter both Email and password',
+          type: 'error',
+        });
+        return;
+      }
+
+      console.log('email:', email);
+      console.log('password:', password);
+
+      const response = await Login_Api(email, password);
+      console.log(response);
+      if (response.msg === 'User logged in successfully.') {
+        await AsyncStorage.removeItem('authToken');
+        await AsyncStorage.setItem('authToken', response.data.token);
+        await AsyncStorage.setItem('email', response.data.email);
+        await AsyncStorage.setItem('password', response.data.password);
+
+        Toast.show({
+          text1: 'Login Successfull',
+          type: 'success',
+        });
+
+        navigation.navigate('AppDrawer');
+      } else {
+        Toast.show({
+          text1: 'Failed to login',
+          type: 'error',
+        });
+      }
+    } catch (error) {
+      console.log('Login Error:', error);
+      Toast.show({
+        text1: 'Error',
+        type: 'error',
+      });
+    }
+  };
 
   return (
     <Paperprovider style={styles.container}>
